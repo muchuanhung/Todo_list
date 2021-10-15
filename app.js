8ddfd5c3cc53b90b16f5d808c6016c0a4efd8453
muchuan <mu.chuan.hung@gmail.com>
2021-10-15 17:41:35 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars') //載入 handlebars
-
+const Todo = require ('./models/todo')
 const app = express()
 
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
@@ -19,13 +19,20 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+//建立一個名為hbs的樣板引擎並傳入exphbs與相關參數~多了extname才能把預設長檔名改為短檔名
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+//啟用樣板引擎hbs
 app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  //拿到全部 Todo 資料
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index' , { todos }))
+    .catch(error => console.error(error))
 })
+
 
 // 設定 port 3000
 app.listen(3000, () => {

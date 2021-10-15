a0aaeab1a00a38ad69b7b03fe1bb4ab2209cea52
muchuan <mu.chuan.hung@gmail.com>
2021-10-15 23:09:50 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars') //載入 handlebars
-const Todo = require ('./models/todo')
+const Todo = require ('./models/todo') //載入Todo model
+const bodyParser = require('body-parser') //載入bodyParser
 const app = express()
 
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
@@ -23,14 +24,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 //啟用樣板引擎hbs
 app.set('view engine', 'hbs')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// 設定首頁路由
+// 設定首頁路由 Controller所執行的動作類似中控台負責串聯Model跟view
 app.get('/', (req, res) => {
-  //拿到全部 Todo 資料
-  Todo.find()
-    .lean()
-    .then(todos => res.render('index' , { todos }))
-    .catch(error => console.error(error))
+  Todo.find() //從todo model資料庫查找出資料
+    .lean() //把mongoose 的model物件轉換成乾淨的 javascript資料陣列
+    .then(todos => res.render('index' , { todos })) //將資料傳給index前端樣板
+    .catch(error => console.error(error)) //錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+//create功能: 資料庫新增資料
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 

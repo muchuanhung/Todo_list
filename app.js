3f5169ebbb38ae9a9d32d346b6f0053c0ab03e34
muchuan <mu.chuan.hung@gmail.com>
2021-10-15 11:56:19 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars') //載入 handlebars
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
+
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -16,10 +19,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
